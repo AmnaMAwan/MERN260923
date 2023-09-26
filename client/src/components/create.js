510d5e62fd14fe98f22af5b1653724409c016ddb
Amna Malik <125828451+AmnaMAwan@users.noreply.github.com>
2023-09-26 16:51:19 +0500

    Update create.js

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -32,7 +32,7 @@ export default function Create() {
   // When a post request is sent to the create url, we'll add a new record to the database.
   const newPerson = { ...form };
 
-  await fetch("http://localhost:5050/record", {
+  await fetch("http://http://143.198.105.14:5050/record", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
